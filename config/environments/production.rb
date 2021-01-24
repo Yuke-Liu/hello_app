ce308b98e9697d7f6a189aa328cce646a429915c
Yuke Liu <yukeliujack@gmail.com>
2021-01-24 01:47:24 +0000

    "V1"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,8 +118,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['yukeliujack@gmail.com'],
+    :password => ENV['get_info(Richard, 20140428);'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
